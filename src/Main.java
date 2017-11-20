666b008c38a5a9e6e9d55bf44e4646a77c52b454
Mike Hemmje Jahn <bob@Mike-Hemmje-Jahn-Air.local>
2017-11-20 10:40:44 +0100

    add mainMenu

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,18 @@ public class Main {
   public static void main(String[] args) {
 
   }
+
+  private static void mainMenu() {
+    System.out.println("1: Member, 2: Kontigent ");
+    do {
+      switch (input) {
+        case 1:
+          memberMenu();
+        case 2:
+          kontigentMenu();
+        default:
+          System.out.println("Not valid menu number");
+      }
+    } while (true);
+  }
 }
