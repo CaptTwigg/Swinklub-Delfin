c37fd239e58225a9c603232f4978c396b219935d
capttwigg <mike@familien-jahn.dk>
2017-11-28 15:36:32 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,58 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
+
   public static void main(String[] args) throws Exception {
-    ArrayList<Employee> emps = new ArrayList<>();
-    emps.add(new Formand("Bob", "Olsen", "Bob@delfin.dk", "Bigboss", "1234", 5, 5));
-    emps.add(new Kasserer("Bob", "Olsen", "Bob@delfin.dk", "MoneyMan", "1234"));
+    ArrayList<Employee> employees = new ArrayList<>();
+    employees.add(new Formand("Bob", "Olsen", "Bob@delfin.dk", "Bigboss", "1234", 5, 5));
+    employees.add(new Kasserer("Bob", "Olsen", "Bob@delfin.dk", "MoneyMan", "1234"));
+
+    ArrayList<ArrayList<String>> test = new ArrayList<>();
+    ArrayList<String> disciplin = new ArrayList<>();
+    ArrayList<String> result = new ArrayList<>();
+    disciplin.add("Crawl");
+    disciplin.add("Butterfly");
+    test.add(disciplin);
+    result.add("11,56");
+    result.add("11,55");
+    test.add(result);
 
 
-    //login(emps);
-    mainMenu(emps);
+    String[][] array = new String[2][5];
+    array[1][1] = "sdf";
 
+
+    for (String a : test.get(0)) {
+      System.out.printf("%s \t", a);
+    }
+    System.out.println();
+    for (String a : test.get(1)) {
+      System.out.println(a);
+    }
+
+
+//    for (ArrayList row : test) {
+//      for (int j = 0; j < 2; j++){
+//        System.out.printf("%s \t1",row);
+//      }
+//      for(int i = 2; i < row.size(); i++){
+//        System.out.printf("%s \t2", row.get(1));
+//      }
+//      System.out.println();
+//    }
+
+    // mainMenu(employees);
   }
 
-  private static void mainMenu(ArrayList<Employee> emps) throws Exception {
-    Scanner input = new Scanner(System.in);
+  private static void mainMenu(ArrayList<Employee> employees) throws Exception {
     int menuNumber = 0;
-    int temp = login(emps);
+    int employeeType = login(employees);
 
-    if (emps.get(temp) instanceof Formand) menuNumber = 1;
-    if (emps.get(temp) instanceof Kasserer) menuNumber = 2;
+    if (employees.get(employeeType) instanceof Formand) menuNumber = 1;
+    if (employees.get(employeeType) instanceof Kasserer) menuNumber = 2;
+
 
     do {
       switch (menuNumber) {
@@ -36,21 +69,21 @@ public class Main {
     } while (false);
   }
 
-  public static int login(ArrayList<Employee> emps) {
+  public static int login(ArrayList<Employee> employees) {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter username:");
     String user = input.next();
     System.out.println("Enter password");
     String pass = input.next();
     int index = -1;
-    for (int i = 0; i < emps.size(); i++) {
-      if (emps.get(i).checkUsername(user) && emps.get(i).checkPassword(pass)) {
+    for (int i = 0; i < employees.size(); i++) {
+      if (employees.get(i).checkUsername(user) && employees.get(i).checkPassword(pass)) {
         index = i;
       }
     }
-    if(index == -1 ) {
+    if (index == -1) {
       System.out.println("Username or Password wrong");
-      index = login(emps);
+      index = login(employees);
 
     }
     return index;
---
+++ src/test.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class test {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scan = new Scanner(new File("result"));
+//
+//    String line = scan.nextLine();
+//    String[] array = line.split("],");
+//
+//    for(String s : array){
+//      String quick =  s.replaceAll("[\\[\\]]", "");
+//      System.out.println(quick);
+//    }
+//
+//    String lin = array[2];
+//    String replace = lin.replaceAll("[\\[\\]]", "");
+//
+//    System.out.println(replace);
+//    String[] ff = new String[4];
+//    System.out.println(Arrays.toString(ff));
+    System.out.println(scan.nextDouble());
+  }
+}
